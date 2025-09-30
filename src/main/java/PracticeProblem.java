e55697a2beea566cb038c80f4dabbd48a30e86ad
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-09-30 22:55:49 +0000

    make changes and commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,54 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
+        // You can test your methods here
+        int[][] testArray = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        System.out.println("Total sum: " + sum2D(testArray));
+        System.out.println("Row 0 sum: " + sumRow(testArray, 0));
+        System.out.println("Column 1 sum: " + sumColumn(testArray, 1));
+    }
 
-	}
+    // Question 1 method
+    public static int sum2D(int[][] array) {
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                total += array[i][j];
+            }
+        }
+        return total;
+    }
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+    // Question 2 method
+    public static int sumRow(int[][] array, int row) {
+        int sum = 0;
+        for (int j = 0; j < array[row].length; j++) {
+            sum += array[row][j];
+        }
+        return sum;
+    }
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+    // Question 3 method
+    public static int sumColumn(int[][] array, int column) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (column < array[i].length) {
+                sum += array[i][column];
+            }
+        }
+        return sum;
+    }
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+    public static void q4() {
+        //Write question 4 code here
+    }
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+    public static void q5() {
+        //Write question 5 code here
+    }
 }
